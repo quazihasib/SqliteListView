d2d799ecb36eb9a0a7e74d4b35a94842483b7e64
eshad-laptop <quazi.hasib13@gmail.com>
2014-09-25 01:09:34 +0600

    3rd commit

---
+++ src/SqliteDB/ContactListItems.java
@@ -0,0 +1,39 @@
+package SqliteDB;
+
+public class ContactListItems {
+	String slno;
+	String name;
+	String phone;
+
+	public String getSlno() {
+		return slno;
+	}
+
+	public void setSlno(String slno) {
+		this.slno = slno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return  phone +  
+			" " + name ;
+	}
+
+}
